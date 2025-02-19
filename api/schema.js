febc31840b34f5ab3e5367a6470b032e242e0b80
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-02-20 05:13:28 +0530

    tried to correct to photo issue

--- api/schema.js
+++ api/schema.js
@@ -1,14 +1,26 @@
 const Joi = require('joi');
+
 module.exports.itemSchema = Joi.object({
-    item: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
+    newSell: Joi.object({
+        title: Joi.object({
+            en: Joi.string().required(),
+            hi: Joi.string().allow('')
+        }).required(),
+        description: Joi.object({
+            en: Joi.string().required(),
+            hi: Joi.string().allow('')
+        }).required(),
         location: Joi.string().required(),
-        pricePerUnit: Joi.number().required().min(0),
-        minOrder: Joi.number().required().min(0),
-        image: Joi.string().allow(" ", null),
+        pricePerUnit: Joi.number().required(),
+        productQuantity: Joi.string().required(),
+        productType: Joi.object({
+            en: Joi.string().required(),
+            hi: Joi.string().allow('')
+        }).required(),
+        minOrder: Joi.number().required(),
+        contactDetail: Joi.string().required()
     }).required()
-})
+});
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
@@ -17,25 +29,25 @@ module.exports.reviewSchema = Joi.object({
     }).required()
 })
 
-module.exports.loanSchema = Joi.object({
-    loan: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.number().required(),
-        bankName: Joi.string().required(),
-        amount: Joi.number().required().min(0),
-        interestRate: Joi.number().required().min(0),
-        duration: Joi.number().required().min(0),
-        bankWebsite: Joi.string().required(),
-        image: Joi.string().allow(" ", null)
-    }).required()
-})
+// module.exports.loanSchema = Joi.object({
+//     loan: Joi.object({
+//         title: Joi.string().required(),
+//         description: Joi.number().required(),
+//         bankName: Joi.string().required(),
+//         amount: Joi.number().required().min(0),
+//         interestRate: Joi.number().required().min(0),
+//         duration: Joi.number().required().min(0),
+//         bankWebsite: Joi.string().required(),
+//         image: Joi.string().allow(" ", null)
+//     }).required()
+// })
 
-module.exports.schemeSchema = Joi.object({
-    scheme: Joi.object({
-        schemeName: Joi.string().required(),
-        description: Joi.string().required(),
-        organization: Joi.string().required(),
-        contactInfo: Joi.string().required(),
-        image: Joi.string().allow(" ", null)
-    }).required()
-})
+// module.exports.schemeSchema = Joi.object({
+//     scheme: Joi.object({
+//         schemeName: Joi.string().required(),
+//         description: Joi.string().required(),
+//         organization: Joi.string().required(),
+//         contactInfo: Joi.string().required(),
+//         image: Joi.string().allow(" ", null)
+//     }).required()
+// })
